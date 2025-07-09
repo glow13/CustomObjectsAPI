111152351e5df1338c11e8c170b861be21d7129b
Garrett Lowrance <garrettlowrance12@gmail.com>
2025-07-09 13:59:25 -0400

    consolidate all api background code
    remove CustomObjectsAPI.hpp for brevity

--- src/CustomObjectsAPI.cpp
+++ src/CustomObjectsAPI.cpp
@@ -1,5 +1,12 @@
-#include "CustomObjectsAPI.hpp"
+#pragma once
+#include <Geode/Geode.hpp>
+#include <Geode/modify/ObjectToolbox.hpp>
+#include <Geode/modify/GameObject.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+
 #include "CustomObjectsManager.hpp"
+
+using namespace geode::prelude;
 
 /*
     For this mod to work you need to make these functions return pointers!!!!
@@ -7,103 +14,123 @@
     GameObject::getObjectTextureRect
 */
 
-bool CustomObjectToolbox::init() {
-    if (!ObjectToolbox::init()) return false;
+class $modify(ObjectToolbox) {
+public:
+    bool init() {
+        if (!ObjectToolbox::init()) return false;
 
-    auto manager = CustomObjectsManager::get();
-    for (int i = 0; i < manager->getObjectCount(); i++) {
-        auto obj = manager->getCustomObject(i);
-        gd::string frame = fmt::format("{}/{}", obj->m_mod, obj->m_spr);
-        m_allKeys.insert(std::pair(obj->m_id, frame));
-    } // for
-
-    manager->printModObjectCount();
-
-    return true;
-} // init
-
-GameObject* GameObjectCO::createWithKey(int key) {
-    if (key >= 4600) {
         auto manager = CustomObjectsManager::get();
-        if (auto obj = manager->getCustomObjectByID(key)) return obj->create();
-    } // if
-    return GameObject::createWithKey(key);
-} // createWithKey
+        for (int i = 0; i < manager->getObjectCount(); i++) {
+            auto obj = manager->getCustomObject(i);
+            gd::string frame = fmt::format("{}/{}", obj->m_mod, obj->m_spr);
+            m_allKeys.insert(std::pair(obj->m_id, frame));
+        } // for
 
-void GJBaseGameLayerCO::setupLayers() {
-    GJBaseGameLayer::setupLayers();
+        manager->printModObjectCount();
+        return true;
+    } // init
+};
 
-    // @geode-ignore(unknown-resource)
-    auto spr = CCSprite::create("CustomObjects.png"_spr)->getTexture();
+class $modify(GameObject) {
+public:
+    static GameObject* createWithKey(int key) {
+        if (key >= 4600) {
+            auto manager = CustomObjectsManager::get();
+            if (auto obj = manager->getCustomObjectByID(key)) return obj->create();
+        } // if
+        return GameObject::createWithKey(key);
+    } // createWithKey
+};
 
-    m_fields->m_customLayerT4 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerT4->setZOrder(m_gameLayerT4->getZOrder());
-    m_fields->m_customLayerT4->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerT4);
+class $modify(GJBaseGameLayer) {
+public:
+    struct Fields {
+        CCSpriteBatchNode* m_customLayerT4;
+        CCSpriteBatchNode* m_customLayerT3;
+        CCSpriteBatchNode* m_customLayerT2;
+        CCSpriteBatchNode* m_customLayerT1;
+        CCSpriteBatchNode* m_customLayerB1;
+        CCSpriteBatchNode* m_customLayerB2;
+        CCSpriteBatchNode* m_customLayerB3;
+        CCSpriteBatchNode* m_customLayerB4;
+        CCSpriteBatchNode* m_customLayerB5;
+    };
 
-    m_fields->m_customLayerT3 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerT3->setZOrder(m_gameLayerT3->getZOrder());
-    m_fields->m_customLayerT3->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerT3);
+    void setupLayers() {
+        GJBaseGameLayer::setupLayers();
 
-    m_fields->m_customLayerT2 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerT2->setZOrder(m_gameLayerT2->getZOrder());
-    m_fields->m_customLayerT2->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerT2);
+        // @geode-ignore(unknown-resource)
+        auto spr = CCSprite::create("CustomObjects.png"_spr)->getTexture();
 
-    m_fields->m_customLayerT1 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerT1->setZOrder(m_gameLayerT1->getZOrder());
-    m_fields->m_customLayerT1->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerT1);
+        m_fields->m_customLayerT4 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerT4->setZOrder(m_gameLayerT4->getZOrder());
+        m_fields->m_customLayerT4->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerT4);
 
-    m_fields->m_customLayerB1 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerB1->setZOrder(m_gameLayerB1->getZOrder());
-    m_fields->m_customLayerB1->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerB1);
+        m_fields->m_customLayerT3 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerT3->setZOrder(m_gameLayerT3->getZOrder());
+        m_fields->m_customLayerT3->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerT3);
 
-    m_fields->m_customLayerB2 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerB2->setZOrder(m_gameLayerB2->getZOrder());
-    m_fields->m_customLayerB2->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerB2);
+        m_fields->m_customLayerT2 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerT2->setZOrder(m_gameLayerT2->getZOrder());
+        m_fields->m_customLayerT2->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerT2);
 
-    m_fields->m_customLayerB3 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerB3->setZOrder(m_gameLayerB3->getZOrder());
-    m_fields->m_customLayerB3->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerB3);
+        m_fields->m_customLayerT1 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerT1->setZOrder(m_gameLayerT1->getZOrder());
+        m_fields->m_customLayerT1->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerT1);
 
-    m_fields->m_customLayerB4 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerB4->setZOrder(m_gameLayerB4->getZOrder());
-    m_fields->m_customLayerB4->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerB4);
+        m_fields->m_customLayerB1 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerB1->setZOrder(m_gameLayerB1->getZOrder());
+        m_fields->m_customLayerB1->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerB1);
 
-    m_fields->m_customLayerB5 = CCSpriteBatchNode::createWithTexture(spr);
-    m_fields->m_customLayerB5->setZOrder(m_gameLayerB5->getZOrder());
-    m_fields->m_customLayerB5->setUseChildIndex(true);
-    m_objectLayer->addChild(m_fields->m_customLayerB5);
-} // setupLayers
+        m_fields->m_customLayerB2 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerB2->setZOrder(m_gameLayerB2->getZOrder());
+        m_fields->m_customLayerB2->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerB2);
 
-CCNode* GJBaseGameLayerCO::parentForZLayer(int zLayer, bool blending, int parentMode, int uiObject) {
-    if (parentMode == 10) {
-        switch(static_cast<ZLayer>(zLayer)) {
-            case ZLayer::T4:
-                return m_fields->m_customLayerT4;
-            case ZLayer::T3:
-                return m_fields->m_customLayerT3;
-            case ZLayer::T2:
-                return m_fields->m_customLayerT2;
-            case ZLayer::T1:
-                return m_fields->m_customLayerT1;
-            case ZLayer::B1:
-                return m_fields->m_customLayerB1;
-            case ZLayer::B2:
-                return m_fields->m_customLayerB2;
-            case ZLayer::B3:
-                return m_fields->m_customLayerB3;
-            case ZLayer::B4:
-                return m_fields->m_customLayerB4;
-            case ZLayer::B5:
-                return m_fields->m_customLayerB5;
-        } // switch
-    } // if
+        m_fields->m_customLayerB3 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerB3->setZOrder(m_gameLayerB3->getZOrder());
+        m_fields->m_customLayerB3->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerB3);
+
+        m_fields->m_customLayerB4 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerB4->setZOrder(m_gameLayerB4->getZOrder());
+        m_fields->m_customLayerB4->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerB4);
+
+        m_fields->m_customLayerB5 = CCSpriteBatchNode::createWithTexture(spr);
+        m_fields->m_customLayerB5->setZOrder(m_gameLayerB5->getZOrder());
+        m_fields->m_customLayerB5->setUseChildIndex(true);
+        m_objectLayer->addChild(m_fields->m_customLayerB5);
+    } // setupLayers
+
+    CCNode* parentForZLayer(int zLayer, bool blending, int parentMode, int uiObject) {
+        if (parentMode == 10) {
+            switch(static_cast<ZLayer>(zLayer)) {
+                case ZLayer::T4:
+                    return m_fields->m_customLayerT4;
+                case ZLayer::T3:
+                    return m_fields->m_customLayerT3;
+                case ZLayer::T2:
+                    return m_fields->m_customLayerT2;
+                case ZLayer::T1:
+                    return m_fields->m_customLayerT1;
+                case ZLayer::B1:
+                    return m_fields->m_customLayerB1;
+                case ZLayer::B2:
+                    return m_fields->m_customLayerB2;
+                case ZLayer::B3:
+                    return m_fields->m_customLayerB3;
+                case ZLayer::B4:
+                    return m_fields->m_customLayerB4;
+                case ZLayer::B5:
+                    return m_fields->m_customLayerB5;
+            } // switch
+        } // if
     return GJBaseGameLayer::parentForZLayer(zLayer, blending, parentMode, uiObject);
-} // parentForZLayer
+    } // parentForZLayer
+};
