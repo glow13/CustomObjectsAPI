3b2f2c2bd6b7aedd0c62fb400c98f4a150bf9752
Garrett Lowrance <garrettlowrance12@gmail.com>
2025-07-18 12:22:10 -0400

    move the manager to its own cpp file
    should have done this a while ago probably

--- include/CustomObjectsAPI.hpp
+++ include/CustomObjectsAPI.hpp
@@ -63,100 +63,22 @@ private:
     Ref<CCDictionary> m_modCustomObjectsDict;
     Ref<CCDictionary> m_modCustomObjectsCount;
 public:
-    static CustomObjectsManager* get() {
-        if (!s_manager) {
-            s_manager = new CustomObjectsManager();
+    static CustomObjectsManager* get();
 
-            s_manager->m_customObjectsCount = 0;
-            s_manager->m_customObjects = CCArray::create();
-            s_manager->m_modCustomObjectsDict = CCDictionary::create();
-            s_manager->m_modCustomObjectsCount = CCDictionary::create();
-        } // if
-        return s_manager;
-    } // get
+    gd::string getCacheDirectory();
+    gd::string getSpritesheetQualityName();
 
-    gd::string getCacheDirectory() {
-        auto path = Mod::get()->getSaveDir().string() + "/custom-objects/";
-        if (!std::filesystem::exists(path)) std::filesystem::create_directory(path);
-        return path;
-    } // getCacheDirectory
+    int getObjectCount() { return m_customObjectsCount; }
+    int getModObjectCount(gd::string id);
+    void incrementModObjectCount(gd::string id);
+    void printModObjectCount();
 
-    gd::string getSpritesheetQualityName() {
-        switch (GameManager::sharedState()->m_texQuality) {
-            case 1: return "CustomObjects";
-            case 2: return "CustomObjects-hd";
-            case 3: return "CustomObjects-uhd";
-        } // switch
-        return "";
-    } // getSpritesheetImagePath
+    ModCustomObject* getCustomObject(int index);
+    ModCustomObject* getCustomObjectByID(int id);
 
-    int getObjectCount() {
-        return m_customObjectsCount;
-    } // getCustomObjectCount
+    void addSpritesheetToCache(gd::string name, Quality quality);
 
-    int getModObjectCount(gd::string id) {
-        auto count = static_cast<CCInteger*>(m_modCustomObjectsCount->objectForKey(id));
-        if (!count) m_modCustomObjectsCount->setObject(CCInteger::create(0), id);
-        return (count) ? count->getValue() : 0;
-    } // getModObjectCount
-
-    void incrementModObjectCount(gd::string id) {
-        auto count = static_cast<CCInteger*>(m_modCustomObjectsCount->objectForKey(id));
-        if (!count) m_modCustomObjectsCount->setObject(CCInteger::create(1), id);
-        else m_modCustomObjectsCount->setObject(CCInteger::create(count->getValue() + 1), id);
-    } // incrementModObjectCount
-
-    ModCustomObject* getCustomObject(int index) {
-        auto obj = m_customObjects->objectAtIndex(index);
-        return static_cast<ModCustomObject*>(obj);
-    } // getCustomObject
-
-    ModCustomObject* getCustomObjectByID(int id) {
-        auto obj = m_modCustomObjectsDict->objectForKey(id);
-        return static_cast<ModCustomObject*>(obj);
-    } // getCustomObjectByID
-
-    void printModObjectCount() {
-        auto keys = m_modCustomObjectsCount->allKeys();
-        for (int i = 0; i < keys->count(); i++) {
-            gd::string mod = static_cast<CCString*>(keys->objectAtIndex(i))->m_sString;
-            int count = static_cast<CCInteger*>(m_modCustomObjectsCount->objectForKey(mod))->getValue();
-            log::info("Mod \"{}\" registered {} custom objects", mod, count);
-        } // for
-    } // printModObjectCount
-
-    void addSpritesheetToCache(gd::string name, Quality quality) {
-        auto spritesheet = CustomObjectsSheet::create(m_customObjects, quality);
-        if (!spritesheet) {
-            log::error("Failed to create spritesheet!!!");
-            return;
-        } // if
-
-        auto image = spritesheet->createSpritesheetImage();
-        auto sheetPath = getCacheDirectory() + name + ".png";
-        bool saved = image->saveToFile(sheetPath.c_str(), false);
-
-        auto dict = spritesheet->createSpritesheetData(name);
-        auto dataPath = getCacheDirectory() + name + ".plist";
-        saved = saved && dict->writeToFile(dataPath.c_str());
-
-        if (saved) log::info("Saved spritesheet to \"{}\"", sheetPath);
-        else log::error("Failed to save spritesheet!!!");
-    } // addSpritesheetToCache
-
-    void registerCustomObject(gd::string spr, CCSize size, std::function<GameObject*(int)> create = CustomGameObject::create) {
-        gd::string mod = spr.substr(0, spr.find("/"));
-        auto obj = new ModCustomObject(spr, getModObjectCount(mod), size * 30, create);
-
-        m_customObjects->addObject(obj);
-        m_modCustomObjectsDict->setObject(obj, obj->m_id);
-
-        m_customObjectsCount++;
-        incrementModObjectCount(mod);
-
-        log::info("Registered custom object with id {}", obj->m_id);
-    } // registerCustomObject
-
+    void registerCustomObject(gd::string spr, CCSize size, std::function<GameObject*(int)> create = CustomGameObject::create);
     void registerCustomObject(gd::string spr, std::function<GameObject*(int)> create = CustomGameObject::create) {
         registerCustomObject(spr, CCSize(1, 1), create);
     } // registerCustomObject
