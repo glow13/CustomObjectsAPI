fdc394aeb20f6c717d6e73fccb419715fd824256
Garrett Lowrance <garrettlowrance12@gmail.com>
2026-01-20 20:30:45 -0500

    finish custom object save data rewrite
    its actually really good now, I was not expecting this

--- include/CustomObjectBase.hpp
+++ include/CustomObjectBase.hpp
@@ -7,6 +7,11 @@ using namespace geode::prelude;
 #define $base(BaseType, ObjectBase) BaseType##Base : public CustomObjectBase<ObjectType, ObjectBase>
 #define $generic(BaseType) BaseType final : public BaseType##Base<BaseType> {}
 
+#define SERIALIZE_DEFS(type, valid, serialize, deserialize) \
+    template<> static bool isValid<type>(type val) { return valid; }; \
+    template<> static std::string serializeValue<type>(type val) { return serialize; }; \
+    template<> static type deserializeValue<type>(std::string val) { return deserialize; };
+
 template <class ObjectType, class ObjectBase>
 class CustomObjectBase : public ObjectBase {
 protected:
@@ -55,15 +60,14 @@ public:
     } // activateCustomObject
 
 protected:
-    template <typename ValueType>
+    template <class ValueType>
     void setupObjectProperty(int key, ValueType& value, std::function<bool(void)> cond = 0) {
-        if (objectProperties.contains(key)) {
-            auto prop = static_cast<ObjectProp<std::string>*>(objectProperties[key]);
-            prop->fromString(&value, prop->value);
-            objectProperties.erase(key);
-            delete prop;
-        } //if
-        objectProperties[key] = new ObjectProp<ValueType>(&value, cond);
+        auto& prop = objectProps.try_emplace(key).first->second;
+        if (!prop.loadedSaveValue.empty()) value = deserializeValue<ValueType>(prop.loadedSaveValue);
+
+        prop.serialize = [&value](){ return serializeValue<ValueType>(value); };
+        prop.deserialize = [&value](std::string str){ value = deserializeValue<ValueType>(str); };
+        prop.isValid = [&value, cond](){ return isValid<ValueType>(value) && (!cond || cond()); };
     } // setupObjectProperty
 
     void addMainSpriteToParent(bool p0) override {
@@ -116,49 +120,35 @@ protected:
     } // resetObject
 
 private:
-    struct ObjectPropBase {
-        virtual void setValue(std::string) = 0;
-        virtual std::string getValue() = 0;
-        virtual bool hasValue() = 0;
-
-        bool valid(bool val) { return val; }
-        bool valid(int val) {return true; }
-        bool valid(float val) { return true; }
-        bool valid(std::string val) { return !val.empty(); }
-
-        std::string toString(bool val) { return val ? "1" : "0"; }
-        std::string toString(int val) { return fmt::format("{}", val); }
-        std::string toString(float val) { return fmt::format("{:g}", val); }
-        std::string toString(std::string val) { return base64::encode(val); }
-
-        void fromString(bool* value, std::string val) { *value = val == "1"; }
-        void fromString(int* value, std::string val) { *value = std::stoi(val); }
-        void fromString(float* value, std::string val) { *value = std::stof(val); }
-        void fromString(std::string* value, std::string val) { *value = base64::decodeString(val).unwrapOrDefault(); }
+    struct ObjectProp final {
+        std::string loadedSaveValue;
+        std::function<std::string()> serialize;
+        std::function<void(std::string)> deserialize;
+        std::function<bool()> isValid;
     };
 
-    template <typename ValueType>
-    struct ObjectProp final : public ObjectPropBase {
-        ValueType* var;
-        ValueType value;
-        std::function<bool(void)> cond;
-        ObjectProp(ValueType* var, std::function<bool(void)> cond, ValueType value = ValueType()) : var(var), value(var ? *var : value), cond(cond) {}
+    template<class ValueType> static bool isValid(ValueType);
+    template<class ValueType> static std::string serializeValue(ValueType);
+    template<class ValueType> static ValueType deserializeValue(std::string);
 
-        std::string getValue() override { return var ? ObjectPropBase::toString(*var) : ""; }
-        bool hasValue() override { return var && ObjectPropBase::valid(*var) && (!cond || cond()); }
-        void setValue(std::string val) override { if (var) ObjectPropBase::fromString(var, val); }
-    };
+    SERIALIZE_DEFS(bool, val, val ? "1" : "0", val == "1");
+    SERIALIZE_DEFS(int, true, std::to_string(val), std::stoi(val));
+    SERIALIZE_DEFS(float, true, std::to_string(val), std::stof(val));
+    SERIALIZE_DEFS(std::string, !val.empty(), base64::encode(val), base64::decodeString(val).unwrapOr(""));
 
     const CustomObjectConfig* config;
-    std::unordered_map<int, ObjectPropBase*> objectProperties;
+    std::unordered_map<int, ObjectProp> objectProps;
 
     gd::string getSaveString(GJBaseGameLayer* p0) override final {
-        std::string propertyString;
-        for (auto [key, prop] : objectProperties) {
-            if (prop->hasValue()) propertyString += fmt::format(",{},{}", key, prop->getValue());
+        auto saveString = ObjectBase::getSaveString(p0);
+
+        for (auto [key, prop] : objectProps) {
+            if (prop.isValid && prop.isValid()) {
+                saveString += fmt::format(",{},{}", key, prop.serialize());
+            } // if
         } // for
 
-        return ObjectBase::getSaveString(p0) + propertyString;
+        return saveString;
     } // getSaveString
 
     void customObjectSetup(gd::vector<gd::string>& propValues, gd::vector<void*>& propIsPresent) override final {
@@ -166,8 +156,10 @@ private:
 
         for (int key = 4; key < propValues.size(); key++) {
             if (!propIsPresent[key]) continue;
-            if (objectProperties.contains(key)) objectProperties[key]->setValue(propValues[key]);
-            else objectProperties[key] = new ObjectProp<std::string>(nullptr, 0, propValues[key]);
+
+            auto& prop = objectProps.try_emplace(key).first->second;
+            if (prop.deserialize) prop.deserialize(propValues[key]);
+            prop.loadedSaveValue = propValues[key];
         } // for
 
         setupCustomObject();
